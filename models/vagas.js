24edb5cd53444270e1ecfbc0e8611f691a845226
Andre Santos <santos.as96@hotmail.com>
2019-07-19 14:04:11 +0100

    bkcabcjac

--- models/vagas.js
+++ models/vagas.js
@@ -6,15 +6,15 @@ module.exports = function(sequelize, Datatypes){
         formation: Datatypes.BOOLEAN,
         travelOtCountrys: Datatypes.BOOLEAN,
         shifts: Datatypes.BOOLEAN,
-        location:{
+        location:Datatypes.INTEGER,/*{
             type: Datatypes.INTEGER,
             references: {
                 model: 'Locations',
                 key: 'id',
                 as: 'location'
             }
-        }, 
-        area: {
+        }, */
+        area: Datatypes.INTEGER/*{
             type: Datatypes.INTEGER,
             references: {
                 model: 'Areas',
@@ -22,10 +22,21 @@ module.exports = function(sequelize, Datatypes){
                 as: 'area'
             }
                     
-        }
+        }*/
         
     });
- 
+    Jobs.associate = function (models) {
+    Jobs.belongsTo(models.Locations,{
+        foreignKey: 'location',
+        constraints: false
+    });
+    }
+    Jobs.associate = function (models) {
+    Jobs.belongsTo(models.Areas,{
+        foreignKey: 'area',
+        constraints: false
+    });
+}
     //Associação com os Utilizadores
     Jobs.associate = function (models) {
         Jobs.belongsToMany(models.Users, {
